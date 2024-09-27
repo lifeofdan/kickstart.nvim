474c474a75ee4a4bdd7d0f7fd7e4162ff56633a0
Daniel Newman <contact@lifeofdan.me>
2024-09-27 13:46:27 -0400

    Install and configure elixir lsp

--- init.lua
+++ init.lua
@@ -618,6 +618,10 @@ require('lazy').setup({
         -- ts_ls = {},
         --
 
+        elixirls = {
+          cmd = {'/root/.elixir-ls/release/language_server.sh'}
+        },
+
         lua_ls = {
           -- cmd = {...},
           -- filetypes = { ...},
@@ -888,7 +892,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'elixir', 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
