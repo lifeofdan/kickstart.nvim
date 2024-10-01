29ace6314e58eea348b5808a38345688ee92479c
Daniel Newman <contact@lifeofdan.me>
2024-10-01 16:30:20 +0000

    add easymotion and easyescape plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,7 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  'easymotion/vim-easymotion',
+  'zhou13/vim-easyescape',
+}
